583fc0769f63d8e1e522d5776d8b0fc5cc14c221
acelyunn <lolicon@Acer-Desktop.localdomain>
2023-12-15 23:51:26 -0600

    save

--- day5.c
+++ day5.c
@@ -34,7 +34,7 @@ void parse(char* input) {
             break;
         }
         if ( '0' <=line[i] && '9' >=line[i]){
-           scanf("%d", &curr, line[i]);
+          sscanf(line[i],"%d", &curr);
           words[curritem][count] = curr;
           i += digit(words[curritem][count]);
         }
